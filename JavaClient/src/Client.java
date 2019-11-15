11da9d0e1def8c955e4185836281898f486910da
Resulkary <resulsaparov@gmail.com>
2019-11-15 20:33:39 +0100

    Implemented Java client.

---
+++ JavaClient/src/Client.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class Client
+{
+    public static void main(String[] args) throws IOException
+    {
+        final String host = "127.0.0.1";
+        final int portNumber = 5555;
+        System.out.println("Creating socket to '" + host + "' on port " + portNumber);
+        Socket socket = null;
+
+        try
+        {
+            socket = new Socket(host, portNumber);
+        }
+        catch (ConnectException e)
+        {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        while (true)
+        {
+            System.out.print("Message: ");
+            BufferedReader inputBR = new BufferedReader(new InputStreamReader(System.in));
+            String input = inputBR.readLine();
+
+            if ("exit".equalsIgnoreCase(input))
+            {
+                socket.close();
+                break;
+            }
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.printf(input);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println(br.readLine());
+        }
+    }
+}
